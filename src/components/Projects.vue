9ee4cf4f10c362ac6457d25e3b877054b6efdefe
jacopo-derrico <derricoj.design@gmail.com>
2024-04-08 17:40:16 +0200

    add api call to projects

---
+++ src/components/Projects.vue
@@ -0,0 +1,40 @@
+<script>
+import axios from 'axios';
+
+export default{
+  name: 'Projects',
+  data(){
+    return{
+      projectArray: [],
+    }
+  },
+  methods: {
+    getProjects() {
+      axios.get( 'http://127.0.0.1:8000/api/projects' )
+        .then(res=>{
+          console.log(res.data.projects)
+          this.projectArray = res.data.projects
+        })
+    }
+  },
+  mounted() {
+    this.getProjects()
+  },
+}
+</script>
+
+<template>
+  <main>
+    <h1>Projects</h1>
+
+    <ul>
+      <li v-for="(proj, index) in projectArray" :key="proj.id">
+        {{ proj.title }}
+      </li>
+    </ul>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
