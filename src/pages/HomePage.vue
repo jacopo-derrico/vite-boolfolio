6d5a0147e8935382dd976ed386562649f635e75c
jacopo-derrico <derricoj.design@gmail.com>
2024-04-11 18:19:48 +0200

    add pages

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,21 @@
+<script>
+
+
+export default {
+  components: {
+    
+  }
+}
+</script>
+
+<template>
+  <main>
+    <div>
+        Benvenuti in home
+    </div>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/pages/ProjectsPage.vue
@@ -0,0 +1,24 @@
+<script>
+import Projects from '../components/Projects.vue'
+
+export default {
+  components: {
+    Projects,
+  }
+}
+</script>
+
+<template>
+  <main>
+    <div>
+        <h1>
+            Progetti
+        </h1>
+    </div>
+    <Projects/>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
